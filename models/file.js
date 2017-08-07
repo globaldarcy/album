e21235d9134986517ebcf057047e3847b30d0668
Shawn <289904316@qq.com>
2017-08-07 14:09:42 +0800

    完成基本功能

--- models/file.js
+++ models/file.js
@@ -2,6 +2,8 @@
  * Created by Shawn on 2017/7/30.
  */
 var fs = require('fs');
+var formidable = require('formidable');
+var path = require("path");
 /*var getAllAlbums = function (callback) {
     fs.readdir("./upload", function (err, files) {
         // var allAlbums = [];
@@ -42,5 +44,40 @@ module.exports = {
             }
             callback(null, files);
         });
+    },
+    upImg : function (req, res, next) {
+        // parse a file upload
+        var form = new formidable.IncomingForm();
+        form.uploadDir = "./upload/";
+
+        form.parse(req, function(err, fields, files) {
+            //console.log(files);
+            if(err){
+                next();
+            }
+            var size = parseInt(files.image.size);
+            if(size > 51200){
+                res.end("图片大于1MB");
+                fs.unlink(files.image.path);
+                return;
+            }
+            var date = Date.now();
+            var ran = parseInt(Math.random() * 89999 + 10000);
+            var extName = path.extname(files.image.name);
+            var folder = fields.folder;
+            var oldPath = files.image.path;
+            var newPath = "./upload/" + folder + "/" + date + ran + extName;
+            form.on('file', function(name, file) {
+                console.log(name);
+                console.log(file);
+            });
+            fs.rename(oldPath, newPath, function (err) {
+                if(err){
+                    return res.rend('改名失败');
+                }
+                return res.redirect('/');
+            });
+        });
+        return;
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 var express = require('express');
+var formidable = require('formidable');
 var router = express.Router();
 var file = require('../models/file');
 
+
+/*var data = [
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "1 convallis timestamp"
+    },
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "2 convallis timestamp 2 Donec "
+    },
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "3 convallis timestamp 2 Donec a fermentum nisi."
+    },
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "4 convallis timestamp 2 Donec a fermentum nisi."
+    },
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "5 convallis timestamp 2 Donec a fermentum nisi."
+    },
+    {
+        img: "https://imgsa.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=f3d4063328738bd4d02cba63c0e2ecb3/a2cc7cd98d1001e910616de1be0e7bec55e797fa.jpg",
+        content: "6 convallis timestamp 2 Donec"
+    }
+];*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   /*res.render('index', {
@@ -19,6 +47,23 @@ router.get('/', function(req, res, next) {
           albums: allAlbums
       })
   });
+});
+/*router.get('/news', function (req, res) {
+    res.json(data);
+});*/
+router.get('/upload', function (req, res, next) {
+    file.getAllAlbums(function (err, allAlbums) {
+        if(err) {
+            next();
+        }
+        res.render('upload', {
+            allAlbums: allAlbums
+        })
+    });
+    //res.redirect("/");
+});
+router.post('/upload', function (req, res, next) {
+    file.upImg(req, res, next);
 });
 router.get('/:albumName', function (req, res, next) {
     file.getAlbumImg(req, res, function (err, allImg) {
